2a2b1f38afb7556c7cc937955e7516a4c2066d5b
YongHwanjae <ist635@naver.com>
2022-12-19 15:31:40 +0900

    게시판 이미지 추가

--- team/src/main/webapp/resources/js/admin/boardManage.js
+++ team/src/main/webapp/resources/js/admin/boardManage.js
@@ -4,3 +4,33 @@ var mySwiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
     },
   });
+
+
+  const boardNotice = document.getElementById("boardNotice");
+  const update = document.getElementById("update");
+  const freeBoard = document.getElementById("freeBoard");
+  const question = document.getElementById("question");
+
+  const swiperMain = document.getElementById("swiperMain");
+  
+  boardNotice.addEventListener("click", ()=>{
+    swiperMain.style.display = "none";
+    // ajax로 공지사항 불러오기  
+  });
+
+  update.addEventListener("click", ()=>{
+    swiperMain.style.display = "none";
+    // 업데이트 불러오기
+  });
+
+  freeBoard.addEventListener("click", ()=>{
+    swiperMain.style.display = "none";
+    // 자유게시판 불러오기
+  });
+
+  question.addEventListener("click", ()=>{
+    swiperMain.style.display = "none";
+    // 문의게시판 불러오기
+  });
+
+
