c251c80a7349f2cbe6b55474299e0b8b8a797df3
Chiwoo <cldn0619@naver.com>
2022-12-20 16:58:31 +0900

    1220

--- team/src/main/java/edu/kh/project/admin/controller/AdminController.java
+++ team/src/main/java/edu/kh/project/admin/controller/AdminController.java
@@ -1,5 +1,6 @@
 package edu.kh.project.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
+	
 
 
 	// 게시글 관리화면 이동
