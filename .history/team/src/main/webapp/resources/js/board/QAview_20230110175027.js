32e06f60877e16c11caccfcb63111b3e2156d94a
YJuY <sd05sd@naver.com>
2023-01-10 17:57:19 +0900

    20230110-엄청느긋해요!

--- .history/team/src/main/webapp/resources/js/board/QAview_20230110175027.js
+++ .history/team/src/main/webapp/resources/js/board/QAview_20230110175027.js
@@ -333,13 +333,14 @@ for (let boardListViewItems of boardListView) {
               boardTitle.innerHTML = saveTitle;
               // 수정될 내용
               boardContent.innerHTML = saveContent;
-  
-              if( QABoardDetail[0].imageList.length==0){
-                contentDetailAnswer.style.display="none";
+              if(QABoardDetail[0].imageList.length = 0){
+                boardViewContentImgArea.style.display='none';
+                contentDetailAnswer.style.display='none';
               }
              //TODO : 이미지 불러오기 / 저장된 이미지
   
                //! 이미지 만드는 create작성해야함.
+               
           if (QABoardDetail[0].imageList.length != 0) {
             boardViewContentImgArea.innerHTML ="";
             ContentImgArea.style.display = "flex";
@@ -349,8 +350,6 @@ for (let boardListViewItems of boardListView) {
             // console.log("이미지번호 : "+ QABoardDetail[0].imgNo);
             // console.log("이미지길이 : "+QABoardDetail[0].imageList.length);
   
-  
-            for (let i = 0; i < QABoardDetail[0].imageList.length; i++) {
               //TODO 아마도 수정 필요
               if (i < 4) {
                 
@@ -363,7 +362,7 @@ for (let boardListViewItems of boardListView) {
                 contentImgDiv.append(contentImgImg);
                 boardViewContentImgArea.append(contentImgDiv);
               }
-            }
+            
           } else {
             ContentImgArea.style.display = "none";
           }
@@ -383,13 +382,17 @@ for (let boardListViewItems of boardListView) {
               // 수정 버튼 클릭 했을때
               boardUpdateInput.addEventListener("click", () => {
                 // console.log("수정버튼 눌림");
+                // console.log(imgArr);
+
                 $.ajax({
                   url: "/QABoardUpdate",
                   type: "GET",
                   data: {
                     boardNo: boardListViewItems.lastElementChild.id,
                     boardContent: boardContent.value,
+                    // imageList: imgArr,
                     boardTitle: boardTitle.value,
+
                   },
                   dataType: "json",
                   success: (result) => {
